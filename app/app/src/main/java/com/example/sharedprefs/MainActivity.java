3cbb7a2423e01aeae9e7e855df3b9e2375b9f170
Gaurav Gupta <gouravgupta840@gmail.com>
2019-08-05 21:52:12 +0530

    ReadWrite to Text File App

--- app/app/src/main/java/com/example/sharedprefs/MainActivity.java
+++ app/app/src/main/java/com/example/sharedprefs/MainActivity.java
@@ -1,54 +1,91 @@
 package com.example.sharedprefs;
 
-import android.content.SharedPreferences;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText enterMessage;
-    private TextView result;
+    private EditText todoInput;
     private Button saveButton;
-
-    private SharedPreferences myPrefs;
-
-    private static final String PREFS_NAME = "myPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        enterMessage = findViewById(R.id.enter_message);
-        result = findViewById(R.id.result_textView);
+        todoInput = findViewById(R.id.todo_input);
 
-        saveButton = findViewById(R.id.save_button);
+        saveButton = findViewById(R.id.savebtn);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 0 - access code for all user
-                myPrefs = getSharedPreferences(PREFS_NAME, 0);
-                SharedPreferences.Editor editor = myPrefs.edit();
-                editor.putString("message", enterMessage.getText().toString());
-                editor.apply();
-
-                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+                String  message = todoInput.getText().toString();
+                if(!TextUtils.isEmpty(message)){
+                    // write if not empty
+                    writeToFile(message);
+                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
-        // Get the data back
-        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
-
-        if (prefs.contains("message")) {
-           String message = prefs.getString("message", "not found");
-           result.setText(message);
+        try {
+            if(readFromFile() != null) {
+                todoInput.setText(readFromFile());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+    }
+
+
+    private void writeToFile(String message) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("todo_list.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(message);
+            outputStreamWriter.close(); // close your stream always. need indeed
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFromFile() throws IOException {
+        String result = "";
+
+        InputStream inputStream =  openFileInput("todo_list.txt");
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String tempString = "";
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((tempString = bufferedReader.readLine()) != null) {
+                stringBuilder.append(tempString);
+            }
+
+            inputStream.close();
+            result = stringBuilder.toString();
+        }
+
+        return result;
     }
 }
